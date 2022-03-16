a6d940d63d11f00578623944f36aa779aef4b2d6
Verilyzed <c.sndr2204@gmail.com>
2022-03-16 16:57:59 +0100

    - Added /time endpoint - Added version

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from datetime import datetime
 app = FastAPI()
 
 MAINTENANCE = False
+VERSION = "0.1.24"
 PRICE_WEED = 10
 PRICE_CRACK = 15
 PRICE_ECSTASY = 20
@@ -17,6 +18,7 @@ PRICE_LSD = 5
 async def data():
     return {
         "maintenance": MAINTENANCE,
+        "version": VERSION,
         "prices": {
             "weed": PRICE_WEED,
             "crack": PRICE_CRACK,
@@ -24,5 +26,14 @@ async def data():
             "heroine": PRICE_HEROINE,
             "lsd": PRICE_LSD
         },
+        "datetime": datetime.utcnow().strftime("%d.%m.%Y %H:%M:%S")
+    }
+
+
+@app.get("/time")
+async def time():
+    return {
+        "maintenance": MAINTENANCE,
+        "version": VERSION,
         "datetime": datetime.utcnow().strftime("%d.%m.%Y %H:%M:%S")
     }
